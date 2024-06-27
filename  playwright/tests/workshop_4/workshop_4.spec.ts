443e3ccec43a54155b4632f33655eae6ddc866c6
omodolapoolabanji <omodolapoolabanji@gmail.com>
2024-06-27 15:44:19 -0400

    recent

---
+++  playwright/tests/workshop_4/workshop_4.spec.ts
@@ -0,0 +1,11 @@
+import {test, expect} from '@playwright/test'
+
+
+//the describe is for clearly grouping test suites
+test.describe('User Registration',  ()=>{
+
+    test('Registering the valid data',async ({page})=>{
+
+        
+    })
+})
